8f94ebb8c65ca521295b332a495ba18db13398a5
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2025-12-16 14:45:23 +0100

    [ADD] generic Deref function to ptr package

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -5,3 +5,13 @@ package ptr
 func Point[T any](v T) *T {
 	return &v
 }
+
+// Deref returns the value pointed to by p.
+// If p is nil, it returns the zero value of type T.
+func Deref[T any](p *T) T {
+	if p == nil {
+		var zero T
+		return zero
+	}
+	return *p
+}
