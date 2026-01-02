bcb9231f9f6d301f04b8386dc8830933a5a30890
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2026-01-02 10:46:56 +0100

    [FIX] move connection state update before handler execution

--- database/database.go
+++ database/database.go
@@ -578,6 +578,10 @@ func (d *Database[Q]) Connect(interval time.Duration, c Config) {
 					copy(handlers, d.onConnectHandler)
 					d.handlerMutex.Unlock()
 
+					d.failed.Store(false)
+					d.connected.Store(true)
+					d.logger.Debug().Msg("connection established")
+
 					for _, handler := range handlers {
 						err := handler(instance, c)
 						if err != nil {
@@ -590,9 +594,6 @@ func (d *Database[Q]) Connect(interval time.Duration, c Config) {
 					if d.failed.Load() {
 						d.logger.Debug().Msg("connection restored")
 					}
-					d.failed.Store(false)
-					d.connected.Store(true)
-					d.logger.Debug().Msg("connection established")
 					return
 				}
 
